5914499d96d658778bb88224cca6dc1f404dc1b6
Dakota Clark <dakotamusic@gmail.com>
2021-06-20 03:14:41 -0400

    use a port object to avoid duplicate ||

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const port = process.env.PORT || process.env.REDIRECT_PORT;
 
 app.get("*", (req, res) => {
   res.redirect(process.env.REDIRECT_URL);
 });
-app.listen(process.env.PORT || process.env.REDIRECT_PORT, () => {
+app.listen(port, () => {
   console.log(
-    `waiting to redirect requests on port ${process.env.REDIRECT_PORT} to ${process.env.REDIRECT_URL}`
+    `waiting to redirect requests on port ${port} to ${process.env.REDIRECT_URL}`
   );
 });
