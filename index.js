8872f95ecfa5b13bc6c1b508ba6c8edc9e0861e4
Dakota Clark <dakotamusic@gmail.com>
2021-06-20 03:04:23 -0400

    let heroku set PORT and defualt to REDIRECT_PORT

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get("*", (req, res) => {
   res.redirect(process.env.REDIRECT_URL);
 });
-app.listen(process.env.REDIRECT_PORT, () => {
+app.listen(process.env.PORT || process.env.REDIRECT_PORT, () => {
   console.log(
     `waiting to redirect requests on port ${process.env.REDIRECT_PORT} to ${process.env.REDIRECT_URL}`
   );
